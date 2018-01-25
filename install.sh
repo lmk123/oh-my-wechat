b088a7b68f351d58083bea3d3e76e87f6d2aad0c
lmk123 <me@limingkai.cn>
2018-01-25 16:28:10 +0800

    安装成功后显示成功消息

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 omw_path="/usr/local/bin/omw"
 curl -o $omw_path https://raw.githubusercontent.com/lmk123/oh-my-wechat/master/main.sh
 chmod 755 $omw_path
+echo "成功安装 oh-my-wechat，现在可以运行 omw 打开微信了。"
